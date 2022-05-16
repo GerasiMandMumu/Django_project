3702bac31e696f264118e302d24f43e968fa4154
GerasiMandMumu <konst.02@yandex.ru>
2022-05-16 20:37:03 +0300

    Добавлены шаблоны, статические файлы, маршрутизация

--- Project1/settings.py
+++ Project1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3bengryo1w)#11&m7_3gl%5dl^-hj4073@4sbkyuq_-fi!5iyc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -118,7 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = []
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- app1/views.py
+++ app1/views.py
@@ -1,21 +1,42 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 
+from .models import *
+
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}]
+
 
 def index(request):
-    return HttpResponse("Страница приложения app1")
+    posts = Women.objects.all()
+    context = {
+        'posts': posts,
+        "title": "Главная",
+        'menu': menu
+    }
+    return render(request, 'app1/index.html', context=context)
 
 
-def categories(request, catid):
-    if request.POST:
-        print(request.POST)
-    return HttpResponse(f"<h1>{catid} app1</h1>")
+def about(request):
+    return render(request, 'app1/about.html', {"title": "О сайте", 'menu': menu})
 
 
-def archive(request, year):
-    if int(year) > 2020:
-        return redirect('home', permanent=True)
-    return HttpResponse(f"<h1>Год {year} app1</h1>")
+def addpage(request):
+    return HttpResponse('Добавление статьи')
+
+
+def contact(request):
+    return HttpResponse('Обратная связь')
+
+
+def login(request):
+    return HttpResponse('Авторизация')
+
+
+def show_post(request, post_id):
+    return HttpResponse(f'Отображение статьи с id = {post_id}')
 
 
 def pageNotFound(request, exception):
