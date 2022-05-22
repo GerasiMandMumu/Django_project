31343a0ac69f83da8dc8d6344f3daa7b2667bb47
GerasiMandMumu <konst.02@yandex.ru>
2022-05-22 15:36:03 +0300

    Работа с формами, связанными с моделями. Пользовательские валидаторы

--- app1/models.py
+++ app1/models.py
@@ -8,7 +8,7 @@ class Women(models.Model):
     content = models.TextField(blank=True, verbose_name='Содержание')
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True, verbose_name='Фото')
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
-    time_update = models.DateTimeField(auto_now=True, verbose_name='Время ихменения')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
     is_published = models.BooleanField(default=True, verbose_name='Статус публикации')
     cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
 
